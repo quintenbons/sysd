5b5d5ee7c3999593fe42a46171f0f473bb07af71
quintenbons <quintenbons@outlook.com>
2023-11-15 01:01:33 +0100

    Directory (use broker for communication between workers)

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Quick start
 
 1. Install RabbitMQ server https://www.rabbitmq.com/install-debian.html with the shell script
-2. Configure it with the scripts/config_rabbit.py tool (it takes optional arguments, check with -h)
-3. Install Celery `pip install "celery[librabbitmq,redis,auth,msgpack]"`
+2. Install Celery and PyAmqp `pip install "celery[librabbitmq,redis,auth,msgpack]" amqp`
+3. Configure it with the `scripts/config_rabbit.py` tool (it takes optional arguments, check with -h)
 4. Launch a Celery worker on your chosen module (eg. for pinpong, within src/, launch `PYTHONPATH=.:$PYTHONPATH celery -A pingpong worker --loglevel=info`)
 5. Go wild (launch `src/pingpong.py` to launch a simple ping pong)
+
+## RabbitMQ Broker
+
+Two vhosts are needed, one for celery (tasks), one for the directory, which is needed for general communication between workers.
+
+`scripts/config_rabbit.py` will configure both hosts automatically. To use the directory, use `src/directory.py`. Keep all amqp communications in that file so that it becomes the abstraction for the amqp interface.
---
+++ src/directory.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Abstract communication interface with the directory
+"""
+from typing import List
+import amqp
+from constants import *
+
+def _new_connection() -> amqp.Connection:
+    return amqp.Connection(
+        host=f"{broker_host}:{broker_port}",
+        userid=directory_user,
+        password=directory_password,
+        virtual_host=directory_vhost,
+        insist=False
+    )
+
+def send_message(queue: str, message: str) -> None:
+    """
+    Sends a message to the given queue.
+    """
+    with _new_connection() as connection, connection.channel() as channel:
+        channel.queue_declare(queue=queue, durable=True)
+        channel.basic_publish(amqp.Message(message), routing_key=queue)
+
+def get_message(queue: str, timeout: int|None = 30) -> List[amqp.Message]:
+    """
+    Blocks until a message is received on the given queue.
+    """
+    messages = []
+
+    with _new_connection() as connection, connection.channel() as channel:
+        def callback(msg: amqp.Message):
+            nonlocal messages
+            messages.append(msg)
+            channel.basic_ack(msg.delivery_tag)
+
+        channel.queue_declare(queue=queue, durable=True)
+        channel.basic_consume(queue=queue, callback=callback)
+        while len(messages) == 0:
+            connection.drain_events(timeout=timeout)
+
+    return messages
+
