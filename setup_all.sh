0cf74416c05d9a42ceaa468a9147721053ec1e75
Julien Bourseau <julien.bourseau@grenoble-inp.org>
2023-11-21 17:25:34 +0100

    new setupall

--- setup_all.sh
+++ setup_all.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-SCRIPT_PATH="deploiement"
-
-chmod +x $SCRIPT_PATH/reserve_nodes.sh
-chmod +x $SCRIPT_PATH/setup_and_deploy.sh
-chmod +x $SCRIPT_PATH/start_workers.sh
-chmod +x $SCRIPT_PATH/cleanup.sh
-
-echo "Réservation des nœuds..."
-# $SCRIPT_PATH/reserve_nodes.sh
-# if [ $? -ne 0 ]; then
-#     echo "Erreur lors de la réservation des nœuds."
-#     exit 1
-# fi
-
-sleep 120
-
-echo "Configuration et déploiement..."
-$SCRIPT_PATH/setup_and_deploy.sh
-if [ $? -ne 0 ]; then
-    echo "Erreur lors de la configuration et du déploiement."
+# Lire le nom d'utilisateur à partir d'un fichier .env
+if [ -f ".env" ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
     exit 1
 fi
 
-echo "Démarrage des workers Celery..."
-$SCRIPT_PATH/start_workers.sh
-if [ $? -ne 0 ]; then
-    echo "Erreur lors du démarrage des workers."
-    exit 1
-fi
+# Connexion à Grid5000
+echo "Connexion à Grid5000"
+ssh -t $USER@access.grid5000.fr "ssh -t grenoble"
+
+# Configuration de l'environnement
+echo "Configuration de l'environnement"
+git clone https://github.com/quintenbons/sysd.git
+cd sysd
+mkdir -p dist
+
+# Installation des dépendances
+echo "Installation de Celery et autres dépendances"
+pip install "celery[librabbitmq,redis,auth,msgpack]" amqp
+
+# Réservation des nœuds
+echo "Réservation des nœuds"
+NODES=2
+WALLTIME="2:00:00"
+oarsub -l nodes=$NODES,walltime=$WALLTIME "sleep 3600"
+
+# Attente que la réservation soit active
+while true; do
+    STATE=$(oarstat -u | grep "$USER" | awk '{print $5}')
+    if [ "$STATE" == "R" ]; then
+        echo "Réservation active."
+        break
+    fi
+    sleep 60  # Attente de 60 secondes avant de vérifier à nouveau
+done
+
+# Récupération des noms d'hôte assignés
+JOB_ID=$(oarstat -u | grep "$USER" | awk '{print $1}')
+NODES=$(oarstat -f -j $JOB_ID | grep "assigned_hostnames" | awk '{print $3}' | tr '+' '\n')
+
+# Commande pour lancer les workers
+WORKER_CMD="export PATH=../:$PATH; celery -A runner worker --loglevel=info"
+
+# Lancer les workers
+for NODE in $NODES; do
+    ssh $USER@$NODE "$WORKER_CMD" &
+done
 
 echo "Déploiement terminé."
