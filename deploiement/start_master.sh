233f2434578addba9de9f6356048c601888c4eb9
Quinten Bons <QuintenBons@outlook.com>
2023-12-05 18:23:42 +0100

    Add example/matrix prep to master tasks

--- deploiement/start_master.sh
+++ deploiement/start_master.sh
@@ -20,4 +20,8 @@ sudo systemctl status rabbitmq-server
 echo "Compiling premier for current architechture..."
 g++ $REPO_PATH/example/premier/premier.c -o $REPO_PATH/bin/premier
 
+echo "Linking sum and multiply..."
+ln -s $REPO_PATH/example/matrix/sum $REPO_PATH/bin/sum
+ln -s $REPO_PATH/example/matrix/multiply $REPO_PATH/bin/multiply
+
 python $REPO_PATH/scripts/config_rabbit.py
