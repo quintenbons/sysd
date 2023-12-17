73d351ebfa585d66309206bfaeaa6025728bcbab
quintenbons <quintenbons@outlook.com>
2023-12-17 19:52:06 +0100

    Fix file paths in flask server

--- src/flask_server.py
+++ src/flask_server.py
@@ -5,16 +5,14 @@ app = Flask(__name__)
 
 @app.route('/files/<filename>', methods=['GET'])
 def serve_file(filename):
-    print(f"Sending file: {filename}")
-    return send_from_directory(os.environ['HOME'], 'sysd', filename)
+    realpath = os.path.realpath(os.path.curdir)
+    return send_from_directory(realpath, filename)
 
 @app.route('/files', methods=['POST'])
 def upload_file():
-    print(f"Pulling file: {request.files['file'].filename}")
     file = request.files['file']
-    file.save(os.path.join(os.environ['HOME'], 'sysd', file.filename))
+    file.save(file.filename)
     return 'File uploaded successfully'
 
 if __name__ == '__main__':
-    print("Listening on port 5000...")
     app.run(host='0.0.0.0', port=5000, threaded=True)
