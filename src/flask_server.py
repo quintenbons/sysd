f3cf8d416a281bb30d07643d9d4ab6f99688c45b
quintenbons <quintenbons@outlook.com>
2023-12-17 19:24:10 +0100

    Flask info

--- src/flask_server.py
+++ src/flask_server.py
@@ -5,13 +5,16 @@ app = Flask(__name__)
 
 @app.route('/files/<filename>', methods=['GET'])
 def serve_file(filename):
+    print(f"Sending file: {filename}")
     return send_from_directory(os.environ['HOME'], 'sysd', filename)
 
 @app.route('/files', methods=['POST'])
 def upload_file():
+    print(f"Pulling file: {request.files['file'].filename}")
     file = request.files['file']
     file.save(os.path.join(os.environ['HOME'], 'sysd', file.filename))
     return 'File uploaded successfully'
 
 if __name__ == '__main__':
+    print("Listening on port 5000...")
     app.run(host='0.0.0.0', port=5000, threaded=True)
