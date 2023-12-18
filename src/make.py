ae3171488e1646bee8a8a2a463052ea3a4429139
Clément Juventin <clementjuventin@gmail.com>
2023-12-18 11:35:30 +0100

    makefile tests

--- src/make.py
+++ src/make.py
@@ -9,6 +9,28 @@ import runner
 import json
 from flask_wrapper import APIWrapper
 
+class PerformanceMeasurement:
+    start_time: int
+    parsing_time: int
+    scheduling_time: int
+    jobs_running_time: []
+    jobs_done_time: []
+    all_jobs_done_time: int
+    pulling_time: int
+    end_time: int
+
+    def to_json(self):
+        return {
+            "start_time": self.start_time,
+            "parsing_time": self.parsing_time,
+            "scheduling_time": self.scheduling_time,
+            "jobs_running_time": self.jobs_running_time,
+            "jobs_done_time": self.jobs_done_time,
+            "all_jobs_done_time": self.all_jobs_done_time,
+            "pulling_time": self.pulling_time,
+            "end_time": self.end_time
+        }
+
 def get_master_node_ip():
     with open(os.path.expanduser('~/g5k_deploy/info.json'), 'r') as file:
         data = json.load(file)
@@ -36,9 +58,14 @@ def main():
     argparser.add_argument('--perf', help='Measure performance', action='store_true') 
 
     args = argparser.parse_args()
+
+    performance = PerformanceMeasurement()
+    performance.start_time = time.time()
     
     tasks = parsing.parse_makefile(args.makefile)
+    performance.parsing_time = time.time()
     task_graph = parsing.generate_task_graph(tasks)
+    performance.scheduling_time = time.time()
 
     done_tasks = set()
     executing_tasks = set()
@@ -54,6 +81,7 @@ def main():
                 running.remove(job_handle)
                 done_tasks.add(task)
                 executing_tasks.remove(task)
+                performance.jobs_done_time.append(time.time())
 
         if len(to_run) == 0:
             print("All running, waiting for news")
@@ -66,12 +94,26 @@ def main():
             dependencies = list(task_graph[task])
             celery_instance = runner.run.delay(task, cmd, dependencies)
             running.add((task, celery_instance))
+            performance.jobs_running_time.append(time.time())
+
+    performance.all_jobs_done_time = time.time()
 
     if args.no_nfs:
         dest_path = os.path.expanduser('~/make_dist')
         print(f"All tasks done! Pulling artifacts... With nfs to {dest_path}")
         if not nfs_pull_artifacts(dest_path):
             print("Failed to pull artifacts")
+
+    performance.pulling_time = time.time()
+    performance.end_time = time.time()
+
+    if args.perf:
+        with open('perf.txt', 'w') as f:
+            json.dump(performance.to_json(), f)
+            f.write('\n')
+            f.write(f"Env: Nodes: {os.environ['NODES']}, Cores: {os.environ['CORES']}, RAM: {os.environ['RAM']}\n")
+            f.write(f"Makefile: {args.makefile}\n")
+            f.write(f"Tasks: {len(tasks)}\n")
 
 if __name__ == "__main__":
     main()
