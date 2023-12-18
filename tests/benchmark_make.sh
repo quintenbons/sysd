da515d7619fd24390c8a06b676f8a51c4620ed4f
quintenbons <quintenbons@outlook.com>
2023-12-18 14:47:41 +0100

    Del cache for benchmarks

--- tests/benchmark_make.sh
+++ tests/benchmark_make.sh
@@ -6,11 +6,24 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+WORKER_NODES=$(jq -r '.workerNodes | join(" ")' $HOME/g5k_deploy/info.json)
+MASTER_NODE=$(jq -r '.masterNode' $HOME/g5k_deploy/info.json)
+WORK_PATH="/tmp/dist"
+
+function del_tmp_dist {
+    # Remote delete the tmp dist with ssh
+    for node in $WORKER_NODES; do
+        ssh $node "rm -rf $WORK_PATH/*"
+    done
+    ssh $MASTER_NODE "rm -rf $WORK_PATH/*"
+}
+
 # Extract arguments
 num_calls=$1
 file_path=$2
 
 # Loop to call the command X times
 for ((i=0; i<$num_calls; i++)); do
+    del_tmp_dist
     ./sysd/src/make.py "$file_path" --no-nfs --perf --index "$i"
 done
